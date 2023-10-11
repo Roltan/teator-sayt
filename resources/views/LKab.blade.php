b99a4492827f008a1cfcb814586d5066d7ffa61b
Roltan <KhalikoR@yandex.ru>
2023-10-11 21:53:23 +0500

    вёрстка личного кабинета

--- resources/views/LKab.blade.php
+++ resources/views/LKab.blade.php
@@ -4,17 +4,49 @@
     Личный кабинет
 @endsection
 
+@section('links')
+    <link rel="stylesheet" href="{{asset('/css/LKab.css')}}">
+@endsection
+
 @section('mainContent')
     <div class="container">
-        <form action="">
-            <div class="mb-3">
-                <label class="form-label">Логин</label>
-                <input type="text" name="name" class="form-control">
+        <div class="ac">
+            <span>Данные вашего акаунта</span>
+
+            <form action="">
+                @csrf
+                <div class="mb-3">
+                    <label class="form-label">Логин</label>
+                    <input type="text" name="name" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Почта</label>
+                    <input type="email" name="email" class="form-control">
+                </div>
+            </form>
+        </div>
+
+        <div class="tickets">
+            <div class="none-ticket">
+                <span>Вы ещё не покупали билеты</span>
+                <a href="/afisha" class="btn-primary">выбрать предстовлеие</a>
             </div>
-            <div class="mb-3">
-                <label class="form-label">Почта</label>
-                <input type="email" name="email" class="form-control">
+            {{-- <div class="ticket">
+                <img src="http://teator-sayt/images/banerPremier.png" alt="">
+                <div>
+                    <span>Театр. Музыка. Любовь.</span>
+                    <span>14 декабря, 19:30</span>
+                    <span class="chair">1 ряд 1 место</span>
+                </div>
             </div>
-        </form>
+            <div class="ticket">
+                <img src="http://teator-sayt/images/banerPremier.png" alt="">
+                <div>
+                    <span>Театр. Музыка. Любовь.</span>
+                    <span>14 декабря, 19:30</span>
+                    <span class="chair">1 ряд 2 место</span>
+                </div>
+            </div> --}}
+        </div>
     </div>
 @endsection
